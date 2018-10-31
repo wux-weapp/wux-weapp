6b4a125925b901c29784fab743c251a57b2b514d
skyvow <724355924@qq.com>
2018-10-31 17:10:42 +0800

    add checkboxItem & update checkbox

--- dist/checkbox/index.js
+++ dist/checkbox/index.js
@@ -1,5 +1,3 @@
-import { isPresetColor } from '../helpers/colors'
-
 Component({
     externalClasses: ['wux-class'],
     relations: {
@@ -27,6 +25,11 @@ Component({
         checked: {
             type: Boolean,
             value: false,
+            observer(newVal) {
+                this.setData({
+                    inputChecked: newVal,
+                })
+            },
         },
         disabled: {
             type: Boolean,
@@ -35,42 +38,31 @@ Component({
         color: {
             type: String,
             value: 'balanced',
-            observer(newVal) {
-                this.setData({
-                    checkboxColor: isPresetColor(newVal),
-                })
-            },
         },
     },
     data: {
         index: 0,
+        inputChecked: false,
     },
     methods: {
-        checkboxChange() {
-            const { value, checked, index, disabled } = this.data
+        checkboxChange(e) {
+            const { value, index, disabled } = this.data
             const parent = this.getRelationNodes('../checkbox-group/index')[0]
             const item = {
-                checked: !checked,
+                checked: e.detail.checked,
                 value,
                 index,
             }
 
-            if (disabled) {
-                return false
-            }
+            if (disabled) return
 
             parent ? parent.emitEvent(item) : this.triggerEvent('change', item)
         },
-        changeValue(checked = false, index = 0) {
+        changeValue(inputChecked = false, index = 0) {
             this.setData({
-                checked,
+                inputChecked,
                 index,
             })
         },
-    },
-    attached() {
-        this.setData({
-            checkboxColor: isPresetColor(this.data.color),
-        })
     },
 })
--- example/pages/checkbox/index.js
+++ example/pages/checkbox/index.js
@@ -4,6 +4,7 @@ Page({
         value2: ['1'],
         value3: ['1'],
         value4: ['1'],
+        checked: false,
     },
     onChange(field, e) {
         const { value } = e.detail
@@ -31,5 +32,11 @@ Page({
     },
     formSubmit(e) {
         console.log('form发生了submit事件，携带数据为：', e.detail.value)
+    },
+    onCheckboxChange(e) {
+        console.log(e)
+        this.setData({
+            checked: e.detail.checked,
+        })
     },
 })
