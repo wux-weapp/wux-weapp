1d126248f3aafa80fcf69d810d8be326c0817419
skyvow <724355924@qq.com>
2018-12-23 22:58:55 +0800

    update wingblank

--- src/wing-blank/index.js
+++ src/wing-blank/index.js
@@ -1,6 +1,11 @@
-Component({
-    externalClasses: ['wux-class'],
+import baseComponent from '../helpers/baseComponent'
+
+baseComponent({
     properties: {
+        prefixCls: {
+            type: String,
+            value: 'wux-wingblank',
+        },
         size: {
             type: String,
             value: 'default',
@@ -8,6 +13,18 @@ Component({
         bodyStyle: {
             type: String,
             value: '',
+        },
+    },
+    computed: {
+        classes() {
+            const { prefixCls, size } = this.data
+            const wrap = this.classNames(prefixCls, {
+                [`${prefixCls}--${size}`]: size,
+            })
+
+            return {
+                wrap,
+            }
         },
     },
 })
