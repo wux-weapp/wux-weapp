0e72337dfa78d0ee7a99c5419bbcd5b5ecedfa69
skyvow <724355924@qq.com>
2018-12-28 14:21:31 +0800

    update alert

--- example/dist/alert/index.js
+++ example/dist/alert/index.js
@@ -1,9 +1,15 @@
-Component({
-	externalClasses: ['wux-class'],
-    options: {
-        multipleSlots: true,
-    },
+import baseComponent from '../helpers/baseComponent'
+
+baseComponent({
     properties: {
+        prefixCls: {
+            type: String,
+            value: 'wux-alert',
+        },
+        classNames: {
+            type: null,
+            value: 'wux-animate--fadeIn',
+        },
         theme: {
             type: String,
             value: 'balanced',
@@ -28,7 +34,36 @@ Component({
     data: {
         visible: true,
     },
+    computed: {
+        classes() {
+            const { prefixCls, theme } = this.data
+            const wrap = this.classNames(prefixCls, {
+                [`${prefixCls}--${theme}`]: theme,
+            })
+            const hd = `${prefixCls}__hd`
+            const thumb = `${prefixCls}__thumb`
+            const bd = `${prefixCls}__bd`
+            const text = `${prefixCls}__text`
+            const desc = `${prefixCls}__desc`
+            const ft = `${prefixCls}__ft`
+            const closable = `${prefixCls}__closable`
+
+            return {
+                wrap,
+                hd,
+                thumb,
+                bd,
+                text,
+                desc,
+                ft,
+                closable,
+            }
+        },
+    },
     methods: {
+        /**
+         * 关闭时触发的回调函数
+         */
         onClose() {
             if (this.data.closable) {
                 this.setData({
@@ -37,6 +72,9 @@ Component({
             }
             this.triggerEvent('click')
         },
+        /**
+         * 点击事件
+         */
         onClick() {
             this.triggerEvent('click')
         },
