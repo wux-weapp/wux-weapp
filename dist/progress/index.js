60614c2783859b435fb6bc2b78c657e98442c2c5
skyvow <724355924@qq.com>
2018-12-28 16:38:15 +0800

    update progress

--- dist/progress/index.js
+++ dist/progress/index.js
@@ -1,3 +1,4 @@
+import baseComponent from '../helpers/baseComponent'
 import { colors } from '../helpers/colors'
 
 const defaultColors = {
@@ -7,9 +8,12 @@ const defaultColors = {
     success: colors.balanced,
 }
 
-Component({
-    externalClasses: ['wux-class'],
+baseComponent({
     properties: {
+        prefixCls: {
+            type: String,
+            value: 'wux-progress',
+        },
         percent: {
             type: Number,
             value: 0,
@@ -50,6 +54,27 @@ Component({
     data: {
         width: 0,
         style: '',
+    },
+    computed: {
+        classes() {
+            const { prefixCls, shape, status } = this.data
+            const wrap = this.classNames(prefixCls, {
+                [`${prefixCls}--${shape}`]: shape,
+                [`${prefixCls}--${status}`]: status,
+            })
+            const outer = `${prefixCls}__outer`
+            const inner = `${prefixCls}__inner`
+            const bar = `${prefixCls}__bar`
+            const text = `${prefixCls}__text`
+
+            return {
+                wrap,
+                outer,
+                inner,
+                bar,
+                text,
+            }
+        },
     },
     methods: {
         updateStyle(opts = {}) {
