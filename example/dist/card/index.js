6264fe89c955c83c4465cc81b5cb1ce31fa603dc
skyvow <724355924@qq.com>
2018-12-28 15:04:59 +0800

    update card

--- example/dist/card/index.js
+++ example/dist/card/index.js
@@ -1,9 +1,11 @@
-Component({
-    externalClasses: ['wux-class'],
-    options: {
-        multipleSlots: true,
-    },
+import baseComponent from '../helpers/baseComponent'
+
+baseComponent({
     properties: {
+        prefixCls: {
+            type: String,
+            value: 'wux-card',
+        },
         bordered: {
             type: Boolean,
             value: true,
@@ -27,6 +29,31 @@ Component({
         extra: {
             type: String,
             value: '',
+        },
+    },
+    computed: {
+        classes() {
+            const { prefixCls, bordered, full } = this.data
+            const wrap = this.classNames(prefixCls, {
+                [`${prefixCls}--bordered`]: bordered,
+                [`${prefixCls}--full`]: full,
+            })
+            const hd = `${prefixCls}__hd`
+            const content = `${prefixCls}__content`
+            const thumb = `${prefixCls}__thumb`
+            const extra = `${prefixCls}__extra`
+            const bd = `${prefixCls}__bd`
+            const ft = `${prefixCls}__ft`
+
+            return {
+                wrap,
+                hd,
+                content,
+                thumb,
+                extra,
+                bd,
+                ft,
+            }
         },
     },
 })
