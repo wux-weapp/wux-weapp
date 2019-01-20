30132d00d89b1816a0d1714999e80860155bfd8d
skyvow <724355924@qq.com>
2019-01-20 20:28:11 +0800

    update about

--- example/pages/about/index.js
+++ example/pages/about/index.js
@@ -1,20 +1,17 @@
 Page({
 	data: {
-		motto: 'Star me', 
-		github: 'https://github.com/wux-weapp/wux-weapp', 
-		userInfo: {}
+		motto: 'Star me',
+		github: 'https://github.com/wux-weapp/wux-weapp',
+        userInfo: {
+            avatarUrl: 'https://avatars3.githubusercontent.com/u/14972920?s=460&v=4',
+            nickname: 'Skyvow',
+            desc: '我是一个小胖子，没事就摸肚子',
+            url: 'https://github.com/skyvow',
+        },
 	},
-	onLoad() {
-		wx.login({
-			success: () => {
-				wx.getUserInfo({
-					success: (res) => {
-						this.setData({
-							userInfo: res.userInfo
-						})
-					}
-				})
-			}
-		})
-	}
+	clipboard(e) {
+        wx.setClipboardData({
+            data: e.currentTarget.dataset.clipboard,
+        })
+    },
 })
