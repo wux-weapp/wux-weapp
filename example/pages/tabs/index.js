bc052e1d3f53fe5778c82e57352bc9af3cbeb464
Ricky <564001002@qq.com>
2019-05-17 23:20:17 +0800

    修复tabs切换的bug

--- example/pages/tabs/index.js
+++ example/pages/tabs/index.js
@@ -1,24 +1,26 @@
 Page({
     data: {
-        current: 'tab1',
-        tabs: [
-        	{
-        		key: 'tab1',
-        		title: 'Tab 1',
-        		content: 'Content of tab 1',
-        	},
-        	{
-        		key: 'tab2',
-        		title: 'Tab 2',
-        		content: 'Content of tab 2',
-        	},
-        	{
-        		key: 'tab3',
-        		title: 'Tab 3',
-        		content: 'Content of tab 3',
-        	},
-        ],
+        // 和tabs-key对应位Number类型
+        //详细页面也与tabs的key对应
+        current: 1,
+        tabs: [{
+                key: 0,
+                title: 'Tab 0',
+                content: 'Content of tab 0',
+            },
+            {
+                key: 1,
+                title: 'Tab 1',
+                content: 'Content of tab 1',
+            },
+            {
+                key: 2,
+                title: 'Tab 2',
+                content: 'Content of tab 2',
+            }
+        ]
     },
+
     onChange(e) {
         console.log('onChange', e)
         this.setData({
@@ -27,24 +29,26 @@ Page({
     },
     onTabsChange(e) {
         console.log('onTabsChange', e)
-        const { key } = e.detail
-        const index = this.data.tabs.map((n) => n.key).indexOf(key)
-
+        console.log(e)
+        const {
+            key: current,
+        } = e.detail
         this.setData({
-            key,
-            index,
+            current
         })
     },
-    onSwiperChange(e) {
-    	console.log('onSwiperChange', e)
-    	const { current: index, source } = e.detail
-    	const { key } = this.data.tabs[index]
 
-    	if (!!source) {
-	    	this.setData({
-	    		key,
-	    		index,
-	    	})
-    	}
-    },
+
+    onSwiperChange(e) {
+        console.log('onSwiperChange', e)
+        const {
+            current,
+            source
+        } = e.detail
+        if (!!source) {
+            this.setData({
+                current
+            })
+        }
+    }
 })
