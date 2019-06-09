3f7c83c9aa824213df751014776cd07235966819
skyvow <724355924@qq.com>
2019-06-09 19:25:15 +0800

    update example

--- example/pages/checkbox/index.js
+++ example/pages/checkbox/index.js
@@ -4,6 +4,9 @@ Page({
         value2: ['1'],
         value3: ['1'],
         value4: ['1'],
+        value5: ['1'],
+        options: [{ title: 'Java', value: '1' }, { title: 'PHP', value: '2' }],
+        // options: ['1', '2'],
     },
     onChange(field, e) {
         const { value } = e.detail
@@ -28,6 +31,9 @@ Page({
     },
     onChange4(e) {
         this.onChange('value4', e)
+    },
+    onChange5(e) {
+        this.onChange('value5', e)
     },
     formSubmit(e) {
         console.log('form发生了submit事件，携带数据为：', e.detail.value)
