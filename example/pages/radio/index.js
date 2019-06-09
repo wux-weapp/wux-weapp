22e08e86e02492773555dc1dd5b811276f445f36
skyvow <724355924@qq.com>
2019-06-09 19:45:42 +0800

    update example

--- example/pages/radio/index.js
+++ example/pages/radio/index.js
@@ -4,13 +4,16 @@ Page({
         value2: '1',
         value3: '1',
         value4: '1',
+        value5: '1',
+        options: [{ title: 'Java', value: '1' }, { title: 'PHP', value: '2' }],
+        // options: ['1', '2'],
     },
     onChange(field, e) {
         this.setData({
             [field]: e.detail.value
         })
 
-        console.log('radio发生change事件，携带value值为：', e.detail.value)
+        console.log('radio发生change事件，携带value值为：', e.detail)
     },
     onChange1(e) {
         this.onChange('value1', e)
@@ -23,6 +26,9 @@ Page({
     },
     onChange4(e) {
         this.onChange('value4', e)
+    },
+    onChange5(e) {
+        this.onChange('value5', e)
     },
     formSubmit(e) {
         console.log('form发生了submit事件，携带数据为：', e.detail.value)
