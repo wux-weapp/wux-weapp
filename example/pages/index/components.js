739d23d0577486089fa3b1c76070531eaa1d6243
skyvow <724355924@qq.com>
2019-12-25 11:03:21 +0800

    update example

---
+++ example/pages/index/components.js
@@ -0,0 +1,416 @@
+export default [{
+    title: 'Basic 基础组件',
+    children: [{
+        title: 'AnimationGroup',
+        remark: '动画组',
+        url: '/pages/animation-group/index',
+    },
+    {
+        title: 'Button',
+        remark: '按钮',
+        url: '/pages/button/index',
+    },
+    {
+        title: 'Icon',
+        remark: '图标',
+        url: '/pages/icon/index',
+    },
+    {
+        title: 'Utility',
+        remark: '工具类',
+        url: '/pages/utility/index',
+    }],
+},
+{
+    title: 'Layout 布局组件',
+    children: [{
+        title: 'Grid',
+        remark: '宫格',
+        url: '/pages/grid/index',
+    },
+    {
+        title: 'Layout',
+        remark: '栅格布局',
+        url: '/pages/layout/index',
+    },
+    {
+        title: 'WhiteSpace',
+        remark: '上下留白',
+        url: '/pages/white-space/index',
+    },
+    {
+        title: 'WingBlank',
+        remark: '两翼留白',
+        url: '/pages/wing-blank/index',
+    }],
+},
+{
+    title: 'Navigation 导航组件',
+    children: [{
+        title: 'NavBar',
+        remark: '导航栏',
+        url: '/pages/navbar/index',
+    },
+    {
+        title: 'Pagination',
+        remark: '分页器',
+        url: '/pages/pagination/index',
+    },
+    {
+        title: 'Popover',
+        remark: '气泡框',
+        url: '/pages/popover/index',
+    },
+    {
+        title: 'SegmentedControl',
+        remark: '分段器',
+        url: '/pages/segmented-control/index',
+    },
+    {
+        title: 'TabBar',
+        remark: '标签栏',
+        url: '/pages/tabbar/index',
+    },
+    {
+        title: 'Tabs',
+        remark: '标签页',
+        url: '/pages/tabs/index',
+    }],
+},
+{
+    title: 'Data Entry 数据录入',
+    children: [{
+        title: 'Calendar',
+        remark: '日历',
+        url: '/pages/calendar/index',
+    },
+    {
+        title: 'Cascader',
+        remark: '级联选择器',
+        url: '/pages/cascader/index',
+    },
+    {
+        title: 'CascaderPickerView',
+        remark: '级联选择器',
+        url: '/pages/cascader-picker-view/index',
+    },
+    {
+        title: 'Checkbox',
+        remark: '复选框',
+        url: '/pages/checkbox/index',
+    },
+    {
+        title: 'DatePicker',
+        remark: '日期选择器',
+        url: '/pages/date-picker/index',
+    },
+    {
+        title: 'DatePickerView',
+        remark: '日期选择器',
+        url: '/pages/date-picker-view/index',
+    },
+    {
+        title: 'Form',
+        remark: '表单',
+        url: '/pages/form/index',
+    },
+    {
+        title: 'Input',
+        remark: '文本输入框',
+        url: '/pages/input/index',
+    },
+    {
+        title: 'InputNumber',
+        remark: '数字输入框',
+        url: '/pages/input-number/index',
+    },
+    {
+        title: 'MultiPickerView',
+        remark: '多列选择器',
+        url: '/pages/multi-picker-view/index',
+    },
+    {
+        title: 'Picker',
+        remark: '选择器',
+        url: '/pages/picker/index',
+    },
+    {
+        title: 'PickerView',
+        remark: '单列选择器',
+        url: '/pages/picker-view/index',
+    },
+    {
+        title: 'PopupSelect',
+        remark: '下拉框',
+        url: '/pages/popup-select/index',
+    },
+    {
+        title: 'Radio',
+        remark: '单选框',
+        url: '/pages/radio/index',
+    },
+    {
+        title: 'Rater',
+        remark: '评分',
+        url: '/pages/rater/index',
+    },
+    {
+        title: 'SearchBar',
+        remark: '搜索栏',
+        url: '/pages/search-bar/index',
+    },
+    {
+        title: 'Select',
+        remark: '下拉框',
+        url: '/pages/select/index',
+    },
+    {
+        title: 'Selectable',
+        remark: '选框组',
+        url: '/pages/selectable/index',
+    },
+    {
+        title: 'Slider',
+        remark: '滑动选择器',
+        url: '/pages/slider/index',
+    },
+    {
+        title: 'Switch',
+        remark: '滑动开关',
+        url: '/pages/switch/index',
+    },
+    {
+        title: 'Textarea',
+        remark: '多行输入框',
+        url: '/pages/textarea/index',
+    },
+    {
+        title: 'Upload',
+        remark: '上传',
+        url: '/pages/upload/index',
+    }],
+},
+{
+    title: 'Data Display 数据展示',
+    children: [{
+        title: 'Accordion',
+        remark: '手风琴',
+        url: '/pages/accordion/index',
+    },
+    {
+        title: 'Alert',
+        remark: '警告框',
+        url: '/pages/alert/index',
+    },
+    {
+        title: 'Avatar',
+        remark: '头像',
+        url: '/pages/avatar/index',
+    },
+    {
+        title: 'Badge',
+        remark: '徽章',
+        url: '/pages/badge/index',
+    },
+    {
+        title: 'Barcode',
+        remark: '条形码',
+        url: '/pages/barcode/index',
+    },
+    {
+        title: 'Card',
+        remark: '卡片',
+        url: '/pages/card/index',
+    },
+    {
+        title: 'CountDown',
+        remark: '倒计时',
+        url: '/pages/countdown/index',
+    },
+    {
+        title: 'CountUp',
+        remark: '计数器',
+        url: '/pages/countup/index',
+    },
+    {
+        title: 'Divider',
+        remark: '分割线',
+        url: '/pages/divider/index',
+    },
+    {
+        title: 'FabButton',
+        remark: '浮动按钮',
+        url: '/pages/fab-button/index',
+    },
+    {
+        title: 'Image',
+        remark: '图片',
+        url: '/pages/image/index',
+    },
+    {
+        title: 'Index',
+        remark: '索引选择器',
+        url: '/pages/index-list/index',
+    },
+    {
+        title: 'List',
+        remark: '列表',
+        url: '/pages/list/index',
+    },
+    {
+        title: 'Media',
+        remark: '媒体对象',
+        url: '/pages/media/index',
+    },
+    {
+        title: 'NoticeBar',
+        remark: '通告栏',
+        url: '/pages/notice-bar/index',
+    },
+    {
+        title: 'Prompt',
+        remark: '提示消息',
+        url: '/pages/prompt/index',
+    },
+    {
+        title: 'Qrcode',
+        remark: '二维码',
+        url: '/pages/qrcode/index',
+    },
+    {
+        title: 'Steps',
+        remark: '步骤条',
+        url: '/pages/steps/index',
+    },
+    {
+        title: 'Sticky',
+        remark: '吸顶容器',
+        url: '/pages/sticky/index',
+    },
+    {
+        title: 'Tag',
+        remark: '标签',
+        url: '/pages/tag/index',
+    },
+    {
+        title: 'Timeago',
+        remark: '多久之前',
+        url: '/pages/timeago/index',
+    },
+    {
+        title: 'Timeline',
+        remark: '时间轴',
+        url: '/pages/timeline/index',
+    },
+    {
+        title: 'Vcode',
+        remark: '验证码',
+        url: '/pages/vcode/index',
+    }],
+},
+{
+    title: 'Feedback 操作反馈',
+    children: [{
+        title: 'ActionSheet',
+        remark: '上拉菜单',
+        url: '/pages/actionsheet/index',
+    },
+    {
+        title: 'Backdrop',
+        remark: '背景幕',
+        url: '/pages/backdrop/index',
+    },
+    {
+        title: 'Circle',
+        remark: '进度环',
+        url: '/pages/circle/index',
+    },
+    {
+        title: 'Dialog',
+        remark: '对话框',
+        url: '/pages/dialog/index',
+    },
+    {
+        title: 'Gallery',
+        remark: '画廊',
+        url: '/pages/gallery/index',
+    },
+    {
+        title: 'Landscape',
+        remark: '压屏窗',
+        url: '/pages/landscape/index',
+    },
+    {
+        title: 'Loading',
+        remark: '指示器',
+        url: '/pages/loading/index',
+    },
+    {
+        title: 'Notification',
+        remark: '通知',
+        url: '/pages/notification/index',
+    },
+    {
+        title: 'Popup',
+        remark: '弹出框',
+        url: '/pages/popup/index',
+    },
+    {
+        title: 'Progress',
+        remark: '进度条',
+        url: '/pages/progress/index',
+    },
+    {
+        title: 'Skeleton',
+        remark: '骨架屏',
+        url: '/pages/skeleton/index',
+    },
+    {
+        title: 'Spin',
+        remark: '加载中',
+        url: '/pages/spin/index',
+    },
+    {
+        title: 'Toast',
+        remark: '提示框',
+        url: '/pages/toast/index',
+    },
+    {
+        title: 'Toptips',
+        remark: '顶部提示',
+        url: '/pages/toptips/index',
+    }],
+},
+{
+    title: 'Expanding 拓展组件',
+    children: [{
+        title: 'FilterBar',
+        remark: '筛选栏',
+        url: '/pages/filterbar/index',
+    },
+    {
+        title: 'KeyBoard',
+        remark: '数字键盘',
+        url: '/pages/keyboard/index',
+    },
+    {
+        title: 'Refresher',
+        remark: '下拉刷新',
+        url: '/pages/refresher/index',
+    },
+    {
+        title: 'Result',
+        remark: '结果页',
+        url: '/pages/result/index',
+    },
+    {
+        title: 'SwipeAction',
+        remark: '滑动操作',
+        url: '/pages/swipe-action/index',
+    },
+    {
+        title: 'VirtualList',
+        remark: '虚拟列表',
+        url: '/pages/virtual-list/index',
+    }],
+}]
