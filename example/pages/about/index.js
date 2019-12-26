ff27ac31511b2b4e2edaab50ec23e6e40f4506ba
skyvow <724355924@qq.com>
2019-12-26 15:15:42 +0800

    update `about` page of example

--- example/pages/about/index.js
+++ example/pages/about/index.js
@@ -2,8 +2,6 @@ import ad from '../index/ad'
 
 ad({
     data: {
-        motto: 'Star me',
-        github: 'https://github.com/wux-weapp/wux-weapp',
         userInfo: {
             avatarUrl: 'https://avatars3.githubusercontent.com/u/14972920?s=460&v=4',
             nickname: 'Skyvow',
@@ -15,5 +13,17 @@ ad({
         wx.setClipboardData({
             data: e.currentTarget.dataset.clipboard,
         })
+    },
+    onPreview(e) {
+        wx.previewImage({
+            urls: [e.currentTarget.dataset.url],
+        })
+    },
+    onShareAppMessage() {
+        return {
+            title: '一套组件化、可复用、易扩展的微信小程序 UI 组件库',
+            path: '/pages/index/index',
+            imageUrl: 'http://cdn.skyvow.cn/logo.png',
+        }
     },
 })
